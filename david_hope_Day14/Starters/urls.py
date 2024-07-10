7e2a155fc85d68f7c7033a18e6d2b8f23ff82cc6
dhope1 <davidhopekavuma@gmail.com>
2024-07-10 16:06:27 +0300

    Updated

--- david_hope_Day14/Starters/urls.py
+++ david_hope_Day14/Starters/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greet_user, name="greet"),
+    path("display/", views.display, name="display_home")
     ]
--- david_hope_Day14/Starters/views.py
+++ david_hope_Day14/Starters/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hey, there')
+
+def greet_user(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}")
+
+def display (request):
+    return render(request, "Starters/home.html")
