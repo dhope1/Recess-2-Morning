894a011b5afc1fe682c9594cd49c2b5af630c1c7
dhope1 <davidhopekavuma@gmail.com>
2024-07-10 17:32:43 +0300

    Updated

--- david_hope_Day14/david_hope_Day14/settings.py
+++ david_hope_Day14/david_hope_Day14/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Starters',
+    'ToDoList',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- david_hope_Day14/david_hope_Day14/urls.py
+++ david_hope_Day14/david_hope_Day14/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Starters/', include('Starters.urls')),
+    path('ToDoList/', include('ToDoList.urls'))
   
 ]
